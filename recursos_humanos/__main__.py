169385e18efa72a96a26584293efc66d5c2d1fdf
Andres <acpr87@gmail.com>
2020-10-06 21:03:31 +0200

    Adjust email controller class, some methods and properties.

--- recursos_humanos/__main__.py
+++ recursos_humanos/__main__.py
@@ -1,6 +1,10 @@
-from .app import EmailReader
+from .email_controller import EmailController
 
 
 if __name__ == '__main__':
-    app = EmailReader()
-    app.start_app()
+    email_controller = EmailController()
+    # email_controller.send_email(recipients=['pedromtz93@gmail.com'])
+    email_controller.download_all_attachments_by_subject_and_format('cv', '.docx')
+    email_controller.download_all_attachments_by_subject_and_format('cv', '.pdf')
+    # email_controller.download_all_attachments_by_subject('cv')
+    # email_controller.send_email()
---
+++ recursos_humanos/email_controller.py
@@ -0,0 +1,48 @@
+import ezgmail
+
+
+class EmailController:
+
+    INBOX_SEARCH = 'label:inbox '
+
+    def __init__(self):
+        ezgmail.init()
+
+    def send_email(self, recipients, subject='Test subject', body='Probando'):
+        for recipient in recipients:
+            ezgmail.send(recipient=recipient, subject=subject, body=body)
+
+    def _search_emails(self, value):
+        search_query = self.INBOX_SEARCH + value
+        res_threat = ezgmail.search(search_query)
+        # print(ezgmail.summary(res_threat, printInfo=False))
+        return res_threat
+
+    def download_all_attachments_by_subject(self, value):
+        emails = self._search_emails(value)
+        for email_index, email_threat in enumerate(emails):
+            for reply_index, email_message in enumerate(email_threat.messages):
+                print(f'Threat: {email_index} - reply: {reply_index}')
+                print(f'   Adjuntos: {email_message.attachments}')
+                email_message.downloadAllAttachments(downloadFolder=f'cv/{value}_selection_process')
+
+    def download_all_attachments_by_subject_and_format(self, value, file_format='.pdf'):
+        emails = self._search_emails(value)
+        for email_index, email_threat in enumerate(emails):
+            for reply_index, email_message in enumerate(email_threat.messages):
+                print(f'Threat: {email_index} - reply: {reply_index}')
+                print(f'   Adjuntos: {email_message.attachments}')
+
+                attachments = email_message.attachments
+                directory_name = file_format.replace('.', '')
+                for attachment in self.filter_by_extension(files=attachments, extension=file_format):
+                    email_message.downloadAttachment(filename=attachment,
+                                                     downloadFolder=f'cv/{value}_selection_process_{directory_name}')
+
+    def filter_by_extension(self, files, extension):
+        tmp_file_list = [file for file in files if extension in file]
+        return tmp_file_list
+
+
+if __name__ == '__main__':
+    reader = EmailController()
